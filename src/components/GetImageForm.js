c52aa898e9716aa11c326ff5a93dace2316d60eb
Rudy Gonzalez <xrgonzalezx@gmail.com>
2017-08-30 18:34:58 -0400

    cleaned up

--- src/components/GetImageForm.js
+++ src/components/GetImageForm.js
@@ -67,19 +67,19 @@ render() {
   return (
     <div>
       <label htmlFor="rover">Rover</label>
-      <select onChange={this.handleRover} id="rover" value={this.state.rover}>
+      <select onChange={this.handleRover} id="rover">
         <option value="Curiosity">Curiosity</option>
         <option value="Opportunity">Opportunity</option>
         <option value="Spirit">Spirt</option>
       </select>
       <label htmlFor="camera">Camera Type</label>
-      <select onChange={this.handleCamera} id="camera" value={this.state.camera}>
+      <select onChange={this.handleCamera} id="camera">
         <option value="fhaz">FHAZ (Front Hazard)</option>
         <option value="rhaz">RHAZ (Rear Hazard)</option>
         <option value="navcam">NAVCAM (Navigation Cam)</option>
       </select>
       <label htmlFor="sol">Martian Sol: 1000-2000 Make selection</label>
-      <input type="number" onChange={this.handleSol} max="2000" min="1000" value={this.state.value}/>
+      <input type="number" onChange={this.handleSol} max="2000" min="1000"/>
       <GetImageButton handleClick={this.fetchRoverImage}/>
        <ImageDisplay images={this.state.images}/>
     </div>
